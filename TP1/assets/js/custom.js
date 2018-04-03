6ef405055677a42b021ec59c343ba7d1386b1c06
gastonframirez <gastonframirez@gmail.com>
2018-04-03 16:38:31 -0300

    Cambios y agrego secciones
    En estructura del sitio.
    Agrego Plantilla base para regiones.
    Cambio JS de menu.
    Agrego secciones de estadisticas y cuestionnario.

--- TP1/assets/js/custom.js
+++ TP1/assets/js/custom.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	"use strict";
 
-    
+
 
         /*==================================
 
@@ -25,9 +25,9 @@ $(document).ready(function(){
 6. Smooth Scroll spy
 7. Animation support
 ======================================*/
-    
 
-    // 1. Scroll To Top 
+
+    // 1. Scroll To Top
 
 		$(window).on('scroll',function () {
 
@@ -56,7 +56,7 @@ $(document).ready(function(){
 		});
 
     // 2. range js
-        
+
         $( "#slider-range" ).slider({
             range: true,
             min: 0,
@@ -68,10 +68,10 @@ $(document).ready(function(){
         });
         $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
         " - $" + $( "#slider-range" ).slider( "values", 1 ) );
-        
-        
+
+
         // Quantity Buttons Shop
-    
+
         $(".qtyplus").on("click", function(){
         var b = $(this).parents(".quantity-form").find("input.qty"),
                 c = parseInt(b.val(), 10) + 1,
@@ -86,98 +86,13 @@ $(document).ready(function(){
         });
 
 
-    // 3.Countdown timer 
-        
-        function makeTimer() {
-
-                var endTime = new Date("March 7, 2019 12:00:00 PDT");            
-                var endTime = (Date.parse(endTime)) / 1000;
-
-                var now = new Date();
-                var now = (Date.parse(now) / 1000);
-
-                var timeLeft = endTime - now;
-
-                var days = Math.floor(timeLeft / 86400); 
-                var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-                var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-                var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-
-                if (hours < "10") { hours = "0" + hours; }
-                if (minutes < "10") { minutes = "0" + minutes; }
-                if (seconds < "10") { seconds = "0" + seconds; }
-
-                $("#days").html(days + '<span class="camp">Days</span>');
-                $("#hours").html(hours + '<span class="camp">Hour</span>');
-                $("#minutes").html(minutes + '<span class="camp">Minute</span>');
-                $("#seconds").html(seconds + '<span class="camp">Second</span>');       
-
-        }
-        
-        setInterval(function() { makeTimer(); }, 1000);
-
-    // 4. owl carousel
-    
-        // i. #testimonial-carousel
-    
-        
-        var owl=$('#testemonial-carousel');
-        owl.owlCarousel({
-            items:3,
-            margin:0,
-            
-            loop:true,
-            autoplay:true,
-            smartSpeed:1000,
-            
-            //nav:false,
-            //navText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-            
-            dots:true,
-            autoplayHoverPause:true,
-        
-            responsiveClass:true,
-                responsive:{
-                    0:{
-                        items:1
-                    },
-                    640:{
-                        items:1
-                    },
-                    767:{
-                        items:2
-                    },
-                    992:{
-                        items:3
-                    }
-                }
-            
-            
-        });
-
-    // 5. datepicker
-            $('[data-toggle="datepicker"]').datepicker();
-
+  
     // 6. Smooth Scroll spy
-        
+
         $('.header-area').sticky({
            topSpacing:0
         });
-        
-        //=============
 
-        $('li.smooth-menu a').bind("click", function(event) {
-            event.preventDefault();
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top - -1
-            }, 1200,'easeInOutExpo');
-        });
-        
-        $('body').scrollspy({
-            target:'.navbar-collapse',
-            offset:0
-        });
 
     // 7.animation support
 
@@ -193,8 +108,6 @@ $(document).ready(function(){
             $(".about-us-txt button").addClass("animated fadeInDown").css({'opacity':'0'});
 
         });
-        
 
-});	
 
-	
+});
