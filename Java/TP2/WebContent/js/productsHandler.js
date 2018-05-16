b04058c8a8a9687e1b6a54003492d5cd7fda06d2
gastonframirez <gastonframirez@gmail.com>
2018-05-15 21:44:03 -0300

    Agrego comentario para saber como leer datos

--- Java/TP2/WebContent/js/productsHandler.js
+++ Java/TP2/WebContent/js/productsHandler.js
@@ -3,6 +3,10 @@ if (localStorage.productsData == null || localStorage.productsData == "null" || 
         localStorage.productsData = JSON.stringify(productsJSON);
     });
 }
+//else
+//{
+//	var products = JSON.parse(localStorage.productsData); // Para leer los datos del localStorage
+//}
 if (localStorage.categoriesData == null || localStorage.categoriesData == "null" || localStorage.categoriesData == "undefined") {
 	$.getJSON( "js/data/categorias.json", function( categoriesJSON ) {
         localStorage.categoriesData = JSON.stringify(categoriesJSON);
